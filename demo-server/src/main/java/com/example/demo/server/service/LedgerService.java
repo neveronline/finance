02f928ccf1a29f38be5a60ea24cdea272af2127b
neveronline <jun32167@gmail.com>
2020-02-20 16:56:18 -0800

    delete useless code

--- demo-server/src/main/java/com/example/demo/server/service/LedgerService.java
+++ demo-server/src/main/java/com/example/demo/server/service/LedgerService.java
@@ -8,11 +8,32 @@ import java.util.List;
 import java.util.Map;
 
 public interface LedgerService {
+    /**
+     * create buckets for a loan
+     * @param request
+     * @return
+     */
     Boolean createBuckets(CreateBucketRequest request);
 
+    /**
+     * get the balance sum of exsited buckets
+     * @param loanId
+     * @param bucketId
+     * @return
+     */
     Map<String, Double> getBucketSum(Integer loanId, List<String> bucketId);
 
+    /**
+     * get all entries for a specific loan
+     * @param loanId
+     * @return
+     */
     List<EntryResponseVO> getEntriesForALoan(Integer loanId);
 
+    /**
+     * add entries to the record and process the record
+     * @param request
+     * @return
+     */
     Boolean addEntries(AddEntryRequest request);
 }
